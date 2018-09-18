cb0787711f88d3a51e91c56f04274fabf256ed4e
uwx <cadrekucra@gmx.com>
2018-09-17 23:00:38 -0300

    make config more descriptive

--- config.lua
+++ config.lua
@@ -1,6 +1,9 @@
 return {
+    -- Whether or not to enable Git index clean/dirty display
     index_enabled = false,
+    -- Whether or not to enable showing the date and/or time at the right side of the terminal
+    -- screen on the prompt
     date_time_enabled = true,
-    -- http://man7.org/linux/man-pages/man3/strftime.3.html
+    -- Date/time format, see http://man7.org/linux/man-pages/man3/strftime.3.html
     date_time_format = '%B %d, %Y %H:%M:%S',
 }
